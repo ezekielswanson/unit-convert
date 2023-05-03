62b9ab29a06bf80ee3099b2b4e89bbb527a6aee8
ezekielswanson <swansonezekiel@gmail.com>
2023-05-02 21:23:37 -0500

    Updated html

--- main.js
+++ main.js
@@ -2,8 +2,10 @@
 
 let input = document.getElementById("input_number");
 const convertBtn = document.getElementById("convert-btn");
-let numberDisplay = document.getElementById('')
 let baseValue = input.value;
+let numberDisplay = document.getElementById('convertedNumbDisplay');
+let numberDisplayTwo = document.getElementById('convertedNumbDisplay-2');
+let numberDisplayThree = document.getElementById('convertedNumbDisplay-3');
 
 
 
@@ -17,28 +19,41 @@ let baseValue = input.value;
 
 
 
-/* meters to feet 
+//Conversion Numbers 
 
-baseValue / 3.28084
-
-let number = Math.floor(baseValue/3.28084);
-numberConvertd.toFixed(3);
-*/
-
-//run funct handles math
-
-function conversions() {
-    const meterToFeet =  3.281;
-}
+//Feet & Meters 
+const meterToFeet =  3.281;
+const feetToMeters = 0.3048;
 
 
+//Gallon & Liter
+const literToGallon = 0.264172;
+const gallonToLiter = 3.78541;
 
-convertBtn.addEventListener('click', function() {
+
+//Gallon & Liter
+const kilosToPounds = 2.20462;
+const poundsToKilos = 0.453592;
+
+
+convertBtn.addEventListener("click", function() {
     let baseValue = input.value;
-    let convertedFeet = baseValue / 3.28084;
+    
+    numberDisplay.textContent = 
+    `${baseValue} meters = ${(baseValue * meterToFeet).toFixed(3)} feet | 
+    ${baseValue} feet = ${(baseValue * feetToMeters).toFixed(3)} meters`;
 
-     let convertedDisplay = `<span>${baseValue} feet = ${convertedFeet * meterToFeet } meters;
-     </span>`
-})
 
-convertedDisplay.innerHTML;
+    
+    numberDisplayTwo.textContent = ` 
+    ${baseValue} liters = ${(baseValue * literToGallon).toFixed(3)} gallons | 
+    ${baseValue} gallons = ${(baseValue * gallonToLiter).toFixed(3)} liters
+    `
+
+    numberDisplayThree.textContent = `
+    ${baseValue} kilos = ${(baseValue * kilosToPounds).toFixed(3)} pounds | 
+    ${baseValue} pounds = ${(baseValue * poundsToKilos).toFixed(3)} kilos
+    `
+    
+});
+
